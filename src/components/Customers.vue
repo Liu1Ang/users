eb18ddec6a815e85832a18f36ac8395b5cdb2570
liuzhiang <liuzhiang@woyouai.cn>
2019-03-18 12:00:51 +0800

    update4

--- src/components/Customers.vue
+++ src/components/Customers.vue
@@ -1,61 +1,86 @@
 <template>
-  <div class="customers container">
-  	<Alert v-if="alert" v-bind:message="alert"></Alert>
-  	<h1 class="page-header">用户管理系统</h1>
-  	<table class="table table-striped">
-  		<thead>
-  			<tr>
-  				<th>姓名</th>
-  				<th>电话</th>
-  				<th>邮箱</th>
-  				<th></th>
-  			</tr>
-  		</thead>
-  		<tbody>
-  			<tr v-for="customer in customers">
-  				<td>{{customer.name}}</td>
-  				<td>{{customer.phone}}</td>
-  				<td>{{customer.email}}</td>
-  				<td><router-link class="btn btn-default" :to="'/customer/'+customer.id">详情</router-link></td>
-  			</tr>
-  		</tbody>
-  	</table>
-  </div>
+	<div class="customers container">
+		<Alert v-if="alert" v-bind:message="alert"></Alert>
+		<h1 class="page-header">用户管理系统</h1>
+		<input type="text" v-model="search" placeholder="搜素用户" class="search" />
+		<tr></tr>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>姓名</th>
+					<th>电话</th>
+					<th>邮箱</th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="customer in findCustomers">
+					<td>{{customer.name}}</td>
+					<td>{{customer.phone}}</td>
+					<td>{{customer.email}}</td>
+					<td>
+						<router-link class="btn btn-default" :to="'/customer/'+customer.id">详情</router-link>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </template>
 
 <script>
 	import axios from 'axios'
 	import Alert from './Alert'
-export default {
-  name: 'customers',
-	components:{Alert},
-  data () {
-    return {
-    	customers:[],
-    	alert:""
-    }
-  },
-  methods:{
-  	fetchCustomers(){
-  		axios.get("http://localhost:3000/user").then((data)=>{
-  			console.log(data);
-  			this.customers = data.data
-  		})
-  	}
-  },
-  created(){
-  	if(this.$route.query.alert){
-  		this.alert = this.$route.query.alert;
-  	}
-  	this.fetchCustomers();
-  },
-  updated(){
-//	this.fetchCustomers();
-  }
-}
+	export default {
+		name: 'customers',
+		components: {
+			Alert
+		},
+		data() {
+			return {
+				customers: [],
+				alert: "",
+				search: ''
+			}
+		},
+		methods: {
+			fetchCustomers() {
+				axios.get("http://localhost:3000/user").then((data) => {
+					console.log(data);
+					this.customers = data.data
+				})
+			}
+		},
+		created() {
+			if(this.$route.query.alert) {
+				this.alert = this.$route.query.alert;
+			}
+			this.fetchCustomers();
+		},
+		updated() {
+			//	this.fetchCustomers();
+		},
+		computed: {
+			findCustomers: function() {
+				return this.customers.filter((customer) => {
+					return customer.name.match(this.search)
+				})
+			}
+		}
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+	.search {
+		width: 100%;
+		height: 30px;
+		line-height: 30px;
+		padding-left: 5px;
+	}
+	
+	tr {
+		width: 100%;
+		height: 20px;
+		background: transparent;
+	}
 </style>
--- src/components/CustomersDetails.vue
+++ src/components/CustomersDetails.vue
@@ -1,95 +1,114 @@
 <template>
-  <div class="Details container">
-  	<h1 class="page-header">姓名：{{customer.name}}</h1>
-  	<form @submit="nosubmit">
-	  	<ul class="list-group">
-	  		<li class="list-group-item">电话：<input type="text" :disabled="isdisabled" :class="boo?'':'active'" v-model="customer.phone"/></li>
-	  		<li class="list-group-item">邮箱：<input type="text" :disabled="isdisabled" :class="boo?'':'active'" v-model="customer.email"/></li>
-	  	</ul>
-	  	<ul class="list-group">
-	  		<li class="list-group-item">学历：<input type="text" :disabled="isdisabled" :class="boo?'':'active'" v-model="customer.education"/></li>
-	  		<li class="list-group-item">毕业学校：<input type="text" :disabled="isdisabled" :class="boo?'':'active'" v-model="customer.graduationschool"/></li>
-	  		<li class="list-group-item">职业：<input type="text" :disabled="isdisabled" :class="boo?'':'active'" v-model="customer.profession"/></li>
-	  		<li class="list-group-item">简介：<textarea class="form-control" :disabled="isdisabled" rows="6" :class="boo?'':'active'" v-model="customer.profile"></textarea></li>
-	  	</ul>
-	  	<button @click="delet">删除</button>
-	  	<button v-if="isedit" @click="edit">编辑</button>
-	  	<button v-if="!isedit" @click="editdetails">确认</button>
-  	</form>
-  </div>
+	<div class="Details container">
+		<form @submit="nosubmit">
+			<div class="title">
+				<h1 class="page-header">姓名：{{customer.name}}</h1>
+				<div>
+					<button @click="delet">删除</button>
+					<button v-if="isedit" @click="edit">编辑</button>
+					<button v-if="!isedit" @click="editdetails">确认</button>
+				</div>
+			</div>
+			<ul class="list-group">
+				<li class="list-group-item">电话：<input type="text" :disabled="isdisabled" :class="boo?'':'active'" v-model="customer.phone" /></li>
+				<li class="list-group-item">邮箱：<input type="text" :disabled="isdisabled" :class="boo?'':'active'" v-model="customer.email" /></li>
+			</ul>
+			<ul class="list-group">
+				<li class="list-group-item">学历：<input type="text" :disabled="isdisabled" :class="boo?'':'active'" v-model="customer.education" /></li>
+				<li class="list-group-item">毕业学校：<input type="text" :disabled="isdisabled" :class="boo?'':'active'" v-model="customer.graduationschool" /></li>
+				<li class="list-group-item">职业：<input type="text" :disabled="isdisabled" :class="boo?'':'active'" v-model="customer.profession" /></li>
+				<li class="list-group-item">简介：<textarea class="form-control" :disabled="isdisabled" rows="6" :class="boo?'':'active'" v-model="customer.profile"></textarea></li>
+			</ul>
+		</form>
+	</div>
 </template>
 
 <script>
 	import axios from 'axios'
-export default {
-  name: 'Details',
-  data () {
-    return {
-      customer:{},
-      id:'',
-      boo:false,
-      active:'active',
-      isedit:true,
-      isdisabled:true
-    }
-  },
-  methods:{
-  	fetchCustomers(id){
-  		axios.get("http://localhost:3000/user/"+id).then((data)=>{
-  			console.log(data.data);
-  			this.customer = data.data
-  		})
-  	},
-  	delet(){
-  		axios.delete("http://localhost:3000/user/"+this.id).then((data)=>{
-  			this.$router.push('/');
-  		})
-  	},
-  	edit(){
-  		this.isedit = false;
-  	},
-  	nosubmit(e){
-  		e.preventDefault();
-  		this.boo = true;
-  		this.isdisabled = false
-  	},
-  	editdetails(e){
-//			console.log(123);
-				if(!this.customer.name || !this.customer.phone || !this.customer.email){
+	export default {
+		name: 'Details',
+		data() {
+			return {
+				customer: {},
+				id: '',
+				boo: false,
+				active: 'active',
+				isedit: true,
+				isdisabled: true
+			}
+		},
+		methods: {
+			fetchCustomers(id) {
+				axios.get("http://localhost:3000/user/" + id).then((data) => {
+					console.log(data.data);
+					this.customer = data.data
+				})
+			},
+			delet() {
+				axios.delete("http://localhost:3000/user/" + this.id).then((data) => {
+					this.$router.push('/');
+				})
+			},
+			edit() {
+				this.isedit = false;
+			},
+			nosubmit(e) {
+				e.preventDefault();
+				this.boo = true;
+				this.isdisabled = false
+			},
+			editdetails(e) {
+				//			console.log(123);
+				if(!this.customer.name || !this.customer.phone || !this.customer.email) {
 					console.log('请填写相应信息！')
-				}else{
+				} else {
 					let newCustomer = {
-						name:this.customer.name,
-						phone:this.customer.phone,
-						email:this.customer.email,
-						education:this.customer.education,
-						graduationschool:this.customer.graduationschool,
-						profession:this.customer.profession,
-						profile:this.customer.profile
+						name: this.customer.name,
+						phone: this.customer.phone,
+						email: this.customer.email,
+						education: this.customer.education,
+						graduationschool: this.customer.graduationschool,
+						profession: this.customer.profession,
+						profile: this.customer.profile
 					}
-					
-					axios.put("http://localhost:3000/user/"+this.id,newCustomer).then((data)=>{
+
+					axios.put("http://localhost:3000/user/" + this.id, newCustomer).then((data) => {
 						console.log(data);
-						this.$router.push({path:"/",query:{alert:"用户信息修改成功！"}});
+						this.$router.push({
+							path: "/",
+							query: {
+								alert: "用户信息修改成功！"
+							}
+						});
 					})
 				}
 				e.preventDefault();
-  	}
-  },
-  created(){
-  	this.fetchCustomers(this.$route.params.id);
-  	this.id = this.$route.params.id;
-  }
-}
+			}
+		},
+		created() {
+			this.fetchCustomers(this.$route.params.id);
+			this.id = this.$route.params.id;
+		}
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-	.active{
+	.active {
 		border-width: 0 !important;
 	}
-	input:disabled{
+	
+	input:disabled {
 		background: transparent !important;
 	}
-
+	
+	.title {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	
+	.page-header {
+		border-bottom: 0;
+	}
 </style>
